0681ca3da9423d8c309cf7e49014bd4ee2279fbd
chieftaha <chieftaha@gmail.com>
2018-02-09 12:13:25 +0330

    publish

---
+++ static/js/app.js
@@ -0,0 +1,17 @@
+/**
+ * Laravel 0 to 60
+ * -------------------------
+ * Created Negar Jamalifard
+ * n.jamalifard@gmail.com
+ * On 2018-02-04
+ */
+hljs.initHighlightingOnLoad();
+hljs.initLineNumbersOnLoad();
+
+ jQuery(function($){
+
+     $('.sidebar_toggle').on('click',function () {
+         $('#sidebar').toggleClass('is-open');
+     })
+
+ }); //End Of siaf!
